62b9bc531b900adcbdf0a7e9ce08c1c2ab6ca458
evshiron <evshiron@gmail.com>
2016-04-26 22:26:44 +0800

    feat(commands): add login command and store session

--- app.js
+++ app.js
@@ -4,10 +4,17 @@
 const commander = require('commander');
 
 const request = require('request');
+request.defaults({
+    headers: {
+        'user-agent': 'Mozilla/5.0 (xiami-cli)',
+    },
+});
 
 const debug = require('debug')('app');
 
 const Flow = require('node-flow');
+
+const Database = require('./lib/database');
 
 const Util = require('./lib/util');
 
@@ -32,6 +39,10 @@ commander.command('parse <SONG_ID...>')
             debug('url:', url);
 
             var [err, res, body] = yield request(url, {}, cb);
+            if(err) {
+                console.error(err);
+                return;
+            }
 
             const json = JSON.parse(body.replace(/^\s+jsonp\d+\(/, '').replace(/\)$/, ''));
 
@@ -44,6 +55,70 @@ commander.command('parse <SONG_ID...>')
                 console.dir(track);
 
             }
+
+        });
+
+    });
+
+commander.command('login <USERNAME> <PASSWORD>')
+    .action((username, password, command) => {
+
+        Flow(function*(cb, u) {
+
+            const jar = request.jar();
+
+            const homeUrl = 'http://www.xiami.com/';
+
+            var [err, res, body] = yield request(homeUrl, {
+                jar: jar,
+            }, cb);
+
+            if(err) {
+                console.error(err);
+                return;
+            }
+
+            const loginUrl = `https://login.xiami.com/member/login?callback=login`;
+
+            debug('loginUrl:', loginUrl);
+
+            const loginForm = {
+                _xiamitoken: jar.getCookies('http://www.xiami.com/').find((element, index, array) => element.key == '_xiamitoken').value,
+                done: 'http://www.xiami.com',
+                from: 'web',
+                havanaId: '',
+                email: username,
+                password: password,
+                submit: '登 录',
+            };
+
+            debug('loginForm:', loginForm);
+
+            var [err, res, body] = yield request.post(loginUrl, {
+                jar: jar,
+                headers: {
+                    'referer': 'https://login.xiami.com/member/login',
+                },
+                form: loginForm,
+            }, cb);
+
+            if(err) {
+                console.error(err);
+                return;
+            }
+
+            const json = JSON.parse(body.replace(/^\s+login\(/, '').replace(/\)$/, ''));
+
+            debug('json:', json);
+
+            Database.Users.insert({
+                username: username,
+                userId: json.data.user_id,
+                nickname: json.data.nick_name,
+                cookies: jar._jar.serializeSync(),
+            });
+
+            console.log(json.status, json.message);
 
         });
 
